5875f92c5b772c0053288550b8a78dc9f1df38c1
Margarita-Kuznetsova <kmv761@mail.ru>
2020-05-02 08:24:40 +0300

    create knot, print

--- tree.c
+++ tree.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <malloc.h>
 #include <stdlib.h>
 
-#define AAA
-#ifdef AAA
 
 typedef int Data;
 
@@ -18,60 +17,46 @@ struct Node * tree_add(struct Node * tree, Data x);
 
 void tree_print(struct Node * tree);
 
-void tree_destroy (struct Node * tree);
 
 int main ()
 {
-	struct Node * tree=NULL;
+	struct Node * tree = NULL;
 	tree = tree_add(tree, 7);
 	tree = tree_add(tree, 3);
 	tree = tree_add(tree, 2);
 	tree = tree_add(tree, 1);
 	tree = tree_add(tree, 9);
 	tree = tree_add(tree, 5);
-	tree = tree_add(tree, 7);
+	tree = tree_add(tree, 4);
 	tree = tree_add(tree, 6);
 	tree = tree_add(tree, 8);
-	tree_print(tree); // напечатает 1 2 3  5 6 7 8 9
+	tree_print(tree); // напечатает 1 2 3 4 5 6 7 8 9
 	
-	tree_destroy(tree);
-	
-	//free(tree);
 	printf("\n");
 	return 0;
 }
 
-#endif
 
-struct Node * tree_add(struct Node * tree, Data x) {
-	if (tree == NULL) {               // Если дерева нет, то формируем корень
-		tree = (struct Node *)malloc(sizeof(struct Node));              // память под узел
-		tree->val = x;                // поле данных
-		tree->left =  NULL;
-		tree->right = NULL;           // ветви инициализируем пустотой
+struct Node * tree_add (struct Node * tree, Data x) {
+	if (tree == NULL) {                                  // Если дерева нет, то формируем корень
+		struct Node * a = malloc(sizeof(struct Node));   // память под узел
+		a->val = x;                                      // поле данных
+		a->left = a->right = NULL;                       // ветви инициализируем пустотой
+		return a;
 	}
-	else
-		if (x < tree->val)            // условие добавление левого потомка
-			tree->left = tree_add(tree->left, x);
-		else                          // условие добавление правого потомка
-			tree->right = tree_add(tree->right, x);
-	return(tree);
+	if (x < tree->val)                                   // условие добавление левого потомка
+		tree->left = tree_add(tree->left, x);
+	else if (x > tree->val)
+		tree->right = tree_add(tree->right, x);          // условие добавление правого потомка
+	return tree;
 }
 
-void tree_print(struct Node * tree) {
-	if (tree != NULL) {             //Пока не встретится пустой узел
-		tree_print(tree->left);     //Рекурсивная функция для левого поддерева
-		printf("%d ", tree->val);     //Отображаем корень дерева
-		tree_print(tree->right);   //Рекурсивная функция для правого поддерева
-	}
-	else
+
+void tree_print (struct Node * tree) {
+	if (tree == NULL)                           //Пока не встретится пустой узел
 		return;
+	tree_print(tree->left);                     //Рекурсивная функция для левого поддерева
+	printf("%d ", tree->val);                   //Отображаем корень дерева
+	tree_print(tree->right);                    //Рекурсивная функция для правого поддерева
 }
 
-void tree_destroy(struct Node * tree) {
-	if (tree != NULL) {
-		tree_destroy(tree->left);
-		tree_destroy(tree->right);
-		free(tree);
-	}
-}
