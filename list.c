c29344cd65495029c43e26864e5b8914103bd966
Margarita-Kuznetsova <kmv761@mail.ru>
2020-05-03 04:52:29 +0300

    list init

--- list.c
+++ list.c
@@ -9,9 +9,23 @@ struct Node {
 	struct Node * prev;
 };
 
+void list_init(struct Node * list);
 
 int main()
 {
+	struct Node x[10];
+	struct Node * a = malloc(sizeof(struct Node));
+	struct Node * b = malloc(sizeof(struct Node));
+	
+	
+	list_init(a);
+	list_init(b);
 
 	return 0;
+}
+
+
+void list_init(struct Node * list) {
+	list -> next = list;
+	list -> prev = list;
 }
