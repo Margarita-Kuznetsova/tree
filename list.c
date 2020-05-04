1965fb5d62445897091760afc4639fcc94d02b13
Margarita-Kuznetsova <kmv761@mail.ru>
2020-05-04 03:28:23 +0300

    list push/pop, is-empty, clear

--- list.c
+++ list.c
@@ -17,7 +17,22 @@ void list_insert_before(struct Node * list, struct Node * t);
 
 void list_remove(struct Node * t);
 
+struct Node * list_push_front(struct Node * list, Data d);
+
+struct Node * list_push_back(struct Node * list, Data d);
+
+Data list_pop_front(struct Node * list);
+
+Data list_pop_back(struct Node * list);
+
+Data list_delete(struct Node * t);
+
 void list_print (struct Node * list);
+
+int list_is_empty(struct Node * list);
+
+void list_clear(struct Node * list);
+
 
 
 
@@ -53,6 +68,35 @@ int main()
 	list_insert(&x[6], &x[5]);
 	list_print(a);
 	
+	
+	while(!list_is_empty(a))
+		list_remove(a->next);
+		
+
+	for(i = 0; i < 10; i++)
+		list_push_back(a, i);
+	list_print(a);
+	
+	
+	for(i = 0; i < 10; i++)
+		list_push_front(b, list_pop_back(a));
+	list_print(b);
+	
+
+	for(i = 0; i < 10; i++) {
+		list_push_front(a, i);
+		list_pop_front(b);
+	}
+	list_print(a);
+	
+
+	for(i = 0; i < 10; i++)
+		list_push_back(b, list_pop_front(a));
+	list_print(b);
+	
+	
+	list_clear(a);
+	list_clear(b);
 
 	return 0;
 }
@@ -87,8 +131,57 @@ void list_remove(struct Node * t) {
 }
 
 
+struct Node * list_push_front(struct Node * list, Data d) {
+	struct Node * p = NULL;
+	p = malloc(sizeof(struct Node));
+	p -> data = d;
+	list_insert(list, p);
+	return p;
+}
+
+
+struct Node * list_push_back(struct Node * list, Data d) {
+	struct Node * w = NULL;
+	w = malloc(sizeof(struct Node));
+	w -> data = d;
+	list_insert_before(list, w);
+	return w;
+}
+
+
+Data list_pop_front(struct Node * list) {
+	return list_delete(list -> next);
+}
+
+
+Data list_pop_back(struct Node * list) {
+	return list_delete(list -> prev);
+}
+
+
+Data list_delete(struct Node * t) {
+	Data res = t -> data;
+	list_remove(t);
+	free(t);
+	return res;
+}
+
+
 void list_print(struct Node * list) {
 	for(struct Node * p  = list -> next; p != list; p = p -> next)
 		printf("%d ", p -> data);
 	printf("\n");
+}
+
+
+int list_is_empty(struct Node * list) {
+	if (list -> prev == list && list -> next == list)
+		return 1;
+	else
+		return 0;
+}
+
+void list_clear(struct Node * list) {
+	while (!list_is_empty(list))
+		list_pop_front(list);
 }
